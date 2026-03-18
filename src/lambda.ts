d623690a73750d52e613c2c70b4d1792014ba44a
Ajith Kumar <kumarajith1996@gmail.com>
2026-03-18 20:35:57 +0530

    fix: remove deprecated callback param

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { configure as serverlessExpress } from '@codegenie/serverless-express';
-import { APIGatewayProxyEventV2, Callback, Context, Handler } from 'aws-lambda';
+import { APIGatewayProxyEventV2, Context } from 'aws-lambda';
 import express from 'express';
 
-let server: Handler;
+type ServerlessHandler = (event: unknown, context: unknown) => Promise<unknown>;
 
-async function bootstrap(): Promise<Handler> {
+let server: ServerlessHandler;
+
+async function bootstrap(): Promise<ServerlessHandler> {
   const expressApp = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp), {
     logger: ['error', 'warn'],
@@ -17,12 +19,10 @@ async function bootstrap(): Promise<Handler> {
   return serverlessExpress({ app: expressApp });
 }
 
-export const handler: Handler = async (
+export const handler = async (
   event: APIGatewayProxyEventV2,
   context: Context,
-  callback: Callback,
-) => {
+): Promise<unknown> => {
   server = server ?? (await bootstrap());
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return server(event, context, callback);
+  return server(event, context);
 };
